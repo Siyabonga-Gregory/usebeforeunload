b384f97a358c6024115d415bb95b0ab2c84aa688
Siyabonga Gregory <huge.fuze@gmail.com>
2022-01-11 12:01:53 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react';
+import { useBeforeunload } from 'react-beforeunload';
+
+function App() {
+
+  const [value, setValue] = useState('');
+
+  useBeforeunload((event) => {
+      event.preventDefault();
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+         <div>
+           <p>Please, type any word in a field below and then close the browswer to see a confirmation</p>
+         <input onChange={(event) => setValue(event.target.value)} value={value} />
+         </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
